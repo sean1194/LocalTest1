b47b6c7504e94a2e5d3248826604abf12dc5f7d2
Neo Hu <sean1194@163.com>
2013-03-15 15:10:00 +0800

    新建文件夹，项目

--- GitTest1/GitTest1/Form1.Designer.cs
+++ GitTest1/GitTest1/Form1.Designer.cs
@@ -44,11 +44,12 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 40);
+            this.label2.Location = new System.Drawing.Point(26, 230);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 12);
             this.label2.TabIndex = 1;
             this.label2.Text = "第二次提交";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
--- GitTest1/GitTest1/Form1.cs
+++ GitTest1/GitTest1/Form1.cs
@@ -16,5 +16,10 @@ namespace GitTest1
             InitializeComponent();
             this.AcceptButton.ToString();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
